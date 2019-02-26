1076eda09aac961ee3e8627626dc34e333508bf3
MohsinAliShah630 <mohsinalishah630@gmail.com>
2019-02-26 00:28:28 -0800

    DeletedTestPHP

--- TestPHP/check1.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-echo $_SESSION["username"];  //if wehave end the session on some page then it will not be displayed on any other page.
-$_SESSION["class"]="CS";
-echo $_SESSION["class"];
-session_unset();
-?>
--- TestPHP/check2.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-
-echo $_SESSION["username"];  //if wehave end the session on some page then it will not be displayed on any other page.
-?>
